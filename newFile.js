090de03e190070d91215bdd7d49155b023c2677c
GianniCbd <giannicabiddu@gmail.com>
2023-10-03 16:52:13 +0200

    U1-W2-D2

---
+++ newFile.js
@@ -0,0 +1,20 @@
+if (
+  /* SCRIVI QUI LA TUA RISPOSTA */
+  /* ESERCIZIO 8
+    Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
+  */
+  /* SCRIVI QUI LA TUA RISPOSTA */
+  /* ESERCIZIO 9
+    Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
+  */
+  /* SCRIVI QUI LA TUA RISPOSTA */
+  /* ESERCIZIO 8
+    Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
+  */
+  /* SCRIVI QUI LA TUA RISPOSTA */
+  /* ESERCIZIO 9
+    Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
+  */
+  let
+)
+  pari = 10;
